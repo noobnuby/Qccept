ecfc5de946765fe2ab74a569a734052de7e852a8
noobnuby <noobnuby77@gmail.com>
2024-11-23 20:27:29 +0900

    asdf

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pystray
 from PIL import Image
 from lcu_driver import Connector
-import sys, os, webbrowser, subprocess
+import sys, os
+import threading
+
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """Get absolute path to resource, works for dev and for PyInstaller"""
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
@@ -15,15 +17,36 @@ def resource_path(relative_path):
 
 def exit_program():
     icon.stop()
+    connector.stop()
     sys.exit()
 
+connector = Connector()
+
+@connector.ready
+async def connect(connection):
+    print("LCU API is ready to be used.")
+
+
+@connector.close
+async def disconnect(connection):
+    print("The client was closed")
+
+@connector.ws.register("/lol-matchmaking/v1/ready-check", event_types=("UPDATE",))
+async def icon_changed(connection, event):
+    await connection.request("post", "/lol-matchmaking/v1/ready-check/accept")
+    print(f"{event.data}")
+
+
+def start_connector():
+    connector.start()
+
+threading.Thread(target=start_connector, daemon=True).start()
+
 icon = pystray.Icon(
-    'Qccept',
+    "Qccept",
     icon=Image.open(resource_path("icon.ico")),
-    menu=pystray.Menu(
-        pystray.MenuItem('종료', exit_program)
-    ),
-    title='Qccept'
+    title="Qccept",
+    menu=pystray.Menu(pystray.MenuItem("종료", exit_program)),
 )
 
 icon.run()
